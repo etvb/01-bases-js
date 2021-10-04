ef4b1a8c63f139a035844c7bb10daeb6f3b0f01d
etvb <edgardo.torres@hotmail.com>
2021-10-04 18:13:12 -0500

    let,var const

---
+++ src/index.js
@@ -0,0 +1,21 @@
+
+
+
+
+
+let nombre = 'Tony'
+const apellido = 'Stark'
+
+console.log(nombre, apellido)
+
+if( true ) {
+    let nombre = 'skdj'
+    console.log(nombre)
+}
+
+console.log(nombre)
+
+
+
+
+
